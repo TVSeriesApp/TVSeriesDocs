6b0f812b169787a3cb1bc22b01a134106dc5422c
Your Name <you@example.com>
2018-01-18 18:48:51 +0100

    npm start

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 let db = require('./db');
 
-var userController = require('./user/userController');
+var userController = require('./user/userController.js');
 app.use('/users', userController);
 
 module.exports = app;
