54ef3da562f3d30a5e08f23e8fcda9c430ccc1fd
Your Name <you@example.com>
2018-01-21 19:18:23 +0100

    further cleaning the code
    removed useless ./api folder

--- api/controllers/restControllers.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var bodyParser = require('body-parser');
-router.use(bodyParser.urlencoded({ extended: true }));
-
-module.exports = router;
